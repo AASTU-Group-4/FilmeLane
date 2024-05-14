a01fe0b767d8006399a5d4f00c68686c14c89077
DeepBlue <deepblue5991@gmail.com>
2024-05-15 00:33:43 +0300

    improve  db connection

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,17 +1,15 @@
 <?php
 function get_connection() {
-    $servername = "localhost";
+    $servername = "127.0.0.1";
     $username = "root";
     $password = "admin";
     $dbname = "movies_db";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    }catch(Exception $e){
+        die("Connection failed: " . $e->getMessage());
     }
-
+    
     return $conn;
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 	Session_start();
-	
+	require_once 'includes/db_connection.php';
+	get_connection();
 ?>
