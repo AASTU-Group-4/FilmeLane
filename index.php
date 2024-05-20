c8342bf0dd1e8fbdf5715ae590ca050581bc5898
DeepBlue <deepblue5991@gmail.com>
2024-05-20 15:59:29 +0300

    refactor: Add Movie model and search functionality to index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-	Session_start();
 	require_once 'includes/db_connection.php';
+	require_once 'models/Movie.php';
+	Session_start();
 	get_connection();
+
+	$movie = new Movie();
+	$movies = $movie->search('The Matrix', 2);
+	echo '<pre>';
+	print_r($movies);
+	echo '</pre>';
+
+
 ?>
--- models/Movie.php
+++ models/Movie.php
@@ -8,10 +8,13 @@ class Movie {
         $this->conn = curl_init();
     }
 
-    public function search($query) {
+    public function search($query, $page = 1) {
+        $url = API_URL . '3/search/multi?query=' . urlencode($query) . '&include_adult=false&language=en-US&page=' . $page;
         
-        $url = API_URL . '3/search/multi?query=' . urlencode($query) . '&include_adult=false&language=en-US&page=1';
-        
+        return $this->sendRequest($url);
+    }
+
+    private function sendRequest($url) {
         curl_setopt($this->conn, CURLOPT_URL, $url);
         curl_setopt($this->conn, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->conn, CURLOPT_HTTPHEADER, [
@@ -26,8 +29,215 @@ class Movie {
         } else {
             return json_decode($response, true);
         }
+    }
+    public function getMovie($id) {
+        $url = API_URL . '3/movie/' . $id . '?language=en-US';
         
-        curl_close($this->conn);
+        return $this->sendRequest($url);
     }
 
+    public function getTVShow($id) {
+        $url = API_URL . '3/tv/' . $id . '?language=en-US';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieCredits($id) {
+        $url = API_URL . '3/movie/' . $id . '/credits';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowCredits($id) {
+        $url = API_URL . '3/tv/' . $id . '/credits';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieReviews($id) {
+        $url = API_URL . '3/movie/' . $id . '/reviews';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowReviews($id) {
+        $url = API_URL . '3/tv/' . $id . '/reviews';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieVideos($id) {
+        $url = API_URL . '3/movie/' . $id . '/videos';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowVideos($id) {
+        $url = API_URL . '3/tv/' . $id . '/videos';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieImages($id) {
+        $url = API_URL . '3/movie/' . $id . '/images';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowImages($id) {
+        $url = API_URL . '3/tv/' . $id . '/images';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieRecommendations($id) {
+        $url = API_URL . '3/movie/' . $id . '/recommendations';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowRecommendations($id) {
+        $url = API_URL . '3/tv/' . $id . '/recommendations';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieSimilar($id) {
+        $url = API_URL . '3/movie/' . $id . '/similar';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowSimilar($id) {
+        $url = API_URL . '3/tv/' . $id . '/similar';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getMovieGenres() {
+        $url = API_URL . '3/genre/movie/list?language=en-US';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowGenres() {
+        $url = API_URL . '3/genre/tv/list?language=en-US';
+        
+        return $this->sendRequest($url);
+    }   
+
+    public function getMovieByGenre($genreID, $page = 1) {
+        $url = API_URL . '3/discover/movie?with_genres=' . $genreID . '&language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowByGenre($genreID, $page = 1) {
+        $url = API_URL . '3/discover/tv?with_genres=' . $genreID . '&language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }   
+
+    public function getMovieByYear($year, $page = 1) {
+        $url = API_URL . '3/discover/movie?primary_release_year=' . $year . '&language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTVShowByYear($year, $page = 1) {
+        $url = API_URL . '3/discover/tv?first_air_date_year=' . $year . '&language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getPopularMovies($page = 1) {
+        $url = API_URL . '3/movie/popular?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTopRatedMovies($page = 1) {
+        $url = API_URL . '3/movie/top_rated?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getNowPlayingMovies($page = 1) {
+        $url = API_URL . '3/movie/now_playing?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getUpcomingMovies($page = 1) {
+        $url = API_URL . '3/movie/upcoming?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getPopularTVShows($page = 1) {
+        $url = API_URL . '3/tv/popular?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTopRatedTVShows($page = 1) {
+        $url = API_URL . '3/tv/top_rated?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getOnTheAirTVShows($page = 1) {
+        $url = API_URL . '3/tv/on_the_air?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getAiringTodayTVShows($page = 1) {
+        $url = API_URL . '3/tv/airing_today?language=en-US&page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTrendingMovies($page = 1) {
+        $url = API_URL . '3/trending/movie/day?page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTrendingTVShows($page = 1) {
+        $url = API_URL . '3/trending/tv/day?page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getTrendingPeople($page = 1) {
+        $url = API_URL . '3/trending/person/day?page=' . $page;
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getPerson($id) {
+        $url = API_URL . '3/person/' . $id . '?language=en-US';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getPersonCredits($id) {
+        $url = API_URL . '3/person/' . $id . '/combined_credits?language=en-US';
+        
+        return $this->sendRequest($url);
+    }   
+
+    public function getPersonImages($id) {
+        $url = API_URL . '3/person/' . $id . '/images';
+        
+        return $this->sendRequest($url);
+    }
+
+    public function getPersonTaggedImages($id) {
+        $url = API_URL . '3/person/' . $id . '/tagged_images';
+        
+        return $this->sendRequest($url);
+    }
+    
 }
