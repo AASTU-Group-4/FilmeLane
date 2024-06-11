6be68ebf977536599ffa86da6e81012d3899c3e5
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2024-06-12 00:54:32 +0300

    create tv_shows page

---
+++ pages/tv_shows.php
@@ -0,0 +1,19 @@
+<!-- tv shows list page -->
+<?php
+session_start();
+require_once '../views/header.php';
+require_once '../models/Movie.php';
+require_once '../includes/function.php';
+$tvshow = new Movie();
+$pTvshowDetails = $movie->getTVShow(($movie->getPopularTVShows())['results'][rand(0, 19)]['id']);
+$trendingTvshows = $movie->getTvshowListDetails($movie->getTrendingTVShows()['results']);
+$currentYear = date('Y');
+if (isset($_GET['year']) && is_numeric($_GET['year'])) {
+    $selectedYear = intval($_GET['year']);
+} else {
+    $selectedYear = $currentYear;
+}
+$Tvshows = $movie->getTvshowByYear($movie->getTVShowByYear($selectedYear)['results']);
+
+require '../views/movies_view.php';
+
